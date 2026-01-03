b8e875644c0415ee37cde370e478c75f2bcc1399
TeamDman <TeamDman9201@gmail.com>
2026-01-03 12:57:39 -0500

    add check-all script

---
+++ check-all.ps1
@@ -0,0 +1,16 @@
+Write-Host -ForegroundColor Yellow "Running format check..."
+cargo fmt --all # -- --check
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+Write-Host -ForegroundColor Yellow "Running clippy lint check..."
+# cargo clippy --all-targets --all-features -- -D warnings
+cargo clippy --all-features -- -D warnings
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+Write-Host -ForegroundColor Yellow "Running build..."
+cargo build --all-features --quiet
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+Write-Host -ForegroundColor Yellow "Running tests..."
+cargo test --all-features --quiet
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
